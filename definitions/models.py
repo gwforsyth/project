41408e3e86cc136b14837012898706313475f65f
centos <centos@localhost.localdomain>
2014-02-06 18:56:34 +0100

    added more classes

--- definitions/models.py
+++ definitions/models.py
@@ -16,29 +16,41 @@ class DBHandler(): #database handler class
 		return DBHandler.connection.cursor() #make database run each time
 
 	class Gene(): #define class gene
+		gene_id=''
 		gene_symbol=''
 		gene_title=''
-		gene_id=''
-		probelist=[]
-
-	def __init__(self,geneid):
-		self.gene_id=geneid
+		
+		probelist=[] #find out what this means
+	
+	def __init__(self,gene_id):
+		self.gene_id=gene_id
 		db=DBHandler()
 		cursor=db.cursor() #link to database
-		sql='select gene_title, gene_symbol from gene where gene_id =%s' #pull out data about gene
-		cursor.execute(sql,(geneid,))
+		sql='select gene_symbol, gene_title from gene where gene_id =%s' #pull out data about gene
+		cursor.execute(sql,(gene_id))
 		#query database
 		#get result and populate the class fields
 		result=cursor.fetchone()
-		self.gene_title =result[0]
-		self.gene_symbol =result[1]
+		self.gene_symbol =result[0]
+		self.gene_title =result[1]
+		
 		#now fetch the  probes..
-		probesql='select probeid from probe where geneid=%s'
-		#fill in the blanks
+		probesql='select probe_id from probe where gene_id=%s'
+		cursor.execute(probesql,(geneid))
+		result=cursor.fetchone()
+		self.probe_id =result[2]
 
-		for result in cursor fetchall():
+		for result in cursor.fetchall():
 			self.probelist.append(result[0])
 	
-	def get_expression(self): #by variables from sample description, experiment
-
+	#def get_expression(self)
+class Probe():
+		probe_id=''
+		gene_id=''	
 	
+	class Samples(): 
+		sample_id=''
+		cell_type=''
+		tissue=''
+
+	class Gene_expression():
