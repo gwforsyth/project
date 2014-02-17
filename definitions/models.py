494245ff824049b40805c23832cd81eec0ddff13
Gillian Forsyth <g.w.forsyth@dundee.ac.uk>
2014-02-17 14:44:12 +0000

    updated section on finding the average for the expression of a gene. also tried to remove the empty sets from the gene info results for a specified tissue but my code isnt quite right yet.

--- definitions/models.py
+++ definitions/models.py
@@ -45,30 +45,19 @@ class Gene(): #define class gene
 		for result in cursor.fetchall():
 			self.probelist.append(result[0])
 	
-	def get_expression(self,sample_id):
+	def  get__average_expression(self,sample_id):
 		'''retrieves a list of all expression values for this gene for the specified experiment expr list = egen.get_expression('GSM12345')'''	
 		db=DBHandler()
 		cursor=db.cursor()
-		sql='select gene_expression from gene_expression where probe_id=%s and sample_id=%s'
+		sql='select sum(gene_expression)/count(gene_expression) from gene_expression where probe_id=%s and sample_id=%s'
 		exvals=[]
 		for p in self.probelist:
 			try:
 				cursor.execute(sql,(p,sample_id,))	
 				exvals.append(cursor.fetchone()[0])
 			except Exception,e:
-				raise Exception('Error occured retrieving gene_expression data for probe_id %s and sample_id %s:%s'%(p,sample_id,e))
+				raise Exception('Error occured retrieving average gene_expression data for probe_id %s and sample_id %s:%s'%(p,sample_id,e))
 		return exvals
-		
-		#now find the average	
-		average=[]
-		calcaverage='sum(%s)/len(%s)'
-		for ex in exvals:
-			try:
-				cursor.execute(calcaverage,(ex,))
-				average.append(cursor.fetchone()[0])
-			except Exception, e:
-				raise Exception('Error occured calculating the average expression for exvals %s'%(ex,e))		
-		return average
 
 class Samples():
 	sample_id=''
@@ -120,13 +109,14 @@ class cell():
 		'''retrieves the gene and probe information for this cell type in the specified experiment'''
 		db=DBHandler()		
 		cursor=db.cursor()
-		sql='select g.gene_id, gene_title, gene_symbol, p.probe_id from gene_expression e inner join probes p on e.probe_id=p.probe_id inner join gene g on g.gene_id=p.gene_id inner join samples s on s.sample_id=e.sample_id where gene_expression=%s and s.sample_id=%s'
+		sql='select s.sample_id,e.gene_expression, g.gene_id, gene_title, gene_symbol, p.probe_id from gene_expression e inner join probes p on e.probe_id=p.probe_id inner join gene g on g.gene_id=p.gene_id inner join samples s on s.sample_id=e.sample_id where gene_expression=%s and s.sample_id=%s'
 		gene_info=[]
 		for ex in self.experiments:
 			try:
-				cursor.execute(sql,(gene_expression,ex,))	
-				gene_info.append(cursor.fetchall())
+				cursor.execute(sql,(gene_expression,ex,))
+				result=cursor.fetchall()
+				if result>0:	
+					gene_info.append(result)
 			except Exception,e:
 				raise Exception('Error occured retrieving the gene and probe information for sample_id %s and gene_expression %s:%s'%(ex,gene_expression,e))
 		return gene_info
-
